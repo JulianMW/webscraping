28afbd35e9ec4c13882ac6bf7480fedefbb15e3a
Julian Michetti-Wilson <jmichett@Gmail.com>
2018-04-16 19:21:47 -0400

    Update openweather-api.py

--- weather/openweather-api.py
+++ weather/openweather-api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 key = ''   # api key
 fullurl = 'http://api.openweathermap.org/data/2.5/weather?id=' + _id + '&APPID=' + key   # full url
@@ -12,3 +13,12 @@ json_data = r.json()
 
 maximum_temperature = json_data['main']['temp_max']
 print(json.dumps(json_data, indent=4, sort_keys=True))
+
+latitudes = [x for x in range(40,45)]
+for lat in latitudes:
+    fullurl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + str(lat) + '&lon=-79.38&APPID=' + key
+    r = requests.get(fullurl)
+    json_data = r.json()
+    maximum_temperature = json_data['main']['temp_max']
+    print(maximum_temperature)
+    time.sleep(1)
